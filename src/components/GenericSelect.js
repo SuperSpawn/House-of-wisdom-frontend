c8b032d863646d330b0a332b34ace2ee3a3a3f17
SuperSpawn <thearktitan@gmail.com>
2023-04-18 22:15:12 +0300

    1.2

--- src/components/GenericSelect.js
+++ src/components/GenericSelect.js
@@ -1,12 +1,17 @@
 import React from "react";
 
+//styles
+import "../styles/reset.css";
+import "../styles/utils.css";
+import "../styles/GenericSelect.css";
+
 export const GenericSelect = ({ options, handlers }) => {
   const changeHandler = (e) => {
     handlers[e.target.value]();
   };
 
   return (
-    <select onChange={changeHandler}>
+    <select className="GenericSelect" onChange={changeHandler}>
       {options.map((option, index) => (
         <option key={index} value={option}>
           {option}
--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-//const webLink = "http://localhost:5001/";
-const webLink = "https://courageous-gray-life-jacket.cyclic.app/";
+const webLink = "http://localhost:5001/";
+//const webLink = "https://courageous-gray-life-jacket.cyclic.app/";
 const sessionLimit = 24; //hours
 
 export { webLink, sessionLimit };
